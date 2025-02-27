9d7e9f653c6f8f7be5f446d7d3c9b4c33defe14c
Shagan742 <526ssangha@frhsd.com>
2025-02-27 23:24:15 +0000

    finished 2nd page

---
+++ script.js
@@ -0,0 +1,3 @@
+//popovers enabling thing from getbootstrap
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
