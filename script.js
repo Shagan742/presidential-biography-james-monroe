0d13ebf5240cb35488b2e4182f7ac4c89374702a
Shagan742 <526ssangha@frhsd.com>
2025-02-28 23:10:27 +0000

    i made the score that counts how many got right and it works

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ let correctAnswers=['James Monroe', 'james monroe', 'James monroe', 'james Monro
 //getting button from html
 const submit = document.getElementById('submitQuiz');
 
+//default score
+let score=document.getElementById('scoreShower').innerHTML=0;
+
 //making the submit button do things
 submit.addEventListener('click', function(){
     //we get the inputs of the user
@@ -27,6 +30,7 @@ submit.addEventListener('click', function(){
 if(inputQuesOne===correctAnswers[0] || inputQuesOne===correctAnswers[1] || inputQuesOne===correctAnswers[2] || inputQuesOne===correctAnswers[3]) {
     //display check mark next to question
     document.getElementById('checkOrXMarkOne').innerText='✅';
+    score++;
 
 } else {
     document.getElementById('checkOrXMarkOne').innerText='❌';
@@ -36,6 +40,7 @@ if(inputQuesOne===correctAnswers[0] || inputQuesOne===correctAnswers[1] || input
 if(inputQuesTwo===correctAnswers[4] || inputQuesTwo===correctAnswers[5] || inputQuesTwo===correctAnswers[6] || inputQuesTwo===correctAnswers[7]) {
     //display check mark next to question
     document.getElementById('checkOrXMarkTwo').innerText='✅';
+    score++;
 } else {
     document.getElementById('checkOrXMarkTwo').innerText='❌';
 }
@@ -44,25 +49,22 @@ if(inputQuesTwo===correctAnswers[4] || inputQuesTwo===correctAnswers[5] || input
 if(inputQuesThree===correctAnswers[8]) {
     //display check mark next to question
     document.getElementById('checkOrXMarkThree').innerText='✅';
+    score++;
 } else {
     document.getElementById('checkOrXMarkThree').innerText='❌';
 }
 
-//if 4th question's first input matches something in correct answer array, its correct
+//if 4th question's input matches something in correct answer array, its correct
 if(inputQuesFourOne===correctAnswers[9] || inputQuesFourOne===correctAnswers[10] || inputQuesFourOne===correctAnswers[11] || inputQuesFourOne===correctAnswers[12]
-    || inputQuesFourOne===correctAnswers[13] || inputQuesFourOne===correctAnswers[14] || inputQuesFourOne===correctAnswers[15] || inputQuesFourOne===correctAnswers[16] || inputQuesFourOne===correctAnswers[17]) {
+    || inputQuesFourOne===correctAnswers[13] || inputQuesFourOne===correctAnswers[14] || inputQuesFourOne===correctAnswers[15] || inputQuesFourOne===correctAnswers[16] || inputQuesFourOne===correctAnswers[17]
+|| inputQuesFourTwo===correctAnswers[9] || inputQuesFourTwo===correctAnswers[10] || inputQuesFourTwo===correctAnswers[11] || inputQuesFourTwo===correctAnswers[12]
+|| inputQuesFourTwo===correctAnswers[13] || inputQuesFourTwo===correctAnswers[14] || inputQuesFourTwo===correctAnswers[15] || inputQuesFourTwo===correctAnswers[16] || inputQuesFourTwo===correctAnswers[17]) {
     //display check mark next to question
     document.getElementById('checkOrXMarkFour').innerText='✅';
+    document.getElementById('checkOrXMarkFive').innerText='✅';
+    score++;
 } else {
     document.getElementById('checkOrXMarkFour').innerText='❌';
-}
-
-//if 4th question's second input matches something in correct answer array, its correct
-if(inputQuesFourTwo===correctAnswers[9] || inputQuesFourTwo===correctAnswers[10] || inputQuesFourTwo===correctAnswers[11] || inputQuesFourTwo===correctAnswers[12]
-    || inputQuesFourTwo===correctAnswers[13] || inputQuesFourTwo===correctAnswers[14] || inputQuesFourTwo===correctAnswers[15] || inputQuesFourTwo===correctAnswers[16] || inputQuesFourTwo===correctAnswers[17]) {
-    //display check mark next to question
-    document.getElementById('checkOrXMarkFive').innerText='✅';
-} else {
     document.getElementById('checkOrXMarkFive').innerText='❌';
 }
 
@@ -70,8 +72,11 @@ if(inputQuesFourTwo===correctAnswers[9] || inputQuesFourTwo===correctAnswers[10]
 if(inputQuesFive===correctAnswers[18]) {
     //display check mark next to question
     document.getElementById('checkOrXMarkSix').innerText='✅';
+    score++;
 } else {
     document.getElementById('checkOrXMarkSix').innerText='❌';
 }
+
+document.getElementById('scoreShower').innerText = score; // Update the score on the page
 
 })
